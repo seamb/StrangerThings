5be7eaffce0c8cbf99a0d01246303a5382179ecf
“Sean” <seam.b22@gmail.com>
2022-02-25 22:38:34 -0700

    delete posts

--- src/components/IndPost.js
+++ src/components/IndPost.js
@@ -4,8 +4,9 @@ export const IndPost = ({post, token, children, username}) => {
 console.log(post._id, "ID POST");
 console.log(post.isAuthor, "is author of post");
 console.log(post.author.username, "is authors username");
+
     return post 
-    ?<div>
+    ? <div>
         <p>
             <strong>{post.title}</strong> 
             <br/>
@@ -17,8 +18,8 @@ console.log(post.author.username, "is authors username");
         </p>
         {children}
     </div>    
-    : 'Loading Posts...'
-    
+   :null 
+   
 }
 
 // export default IndPost;
